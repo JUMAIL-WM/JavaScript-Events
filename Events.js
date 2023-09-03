fa1f16259842a8c6ef2de04776b0a88c41953968
Your Name <you@example.com>
2023-09-03 11:56:50 +0530

    first commit

---
+++ Events.js
@@ -0,0 +1,53 @@
+function myFunction() {
+    var x = document.getElementById("mySelect").value;
+    document.getElementById("demo").innerHTML = "You selected: " + x;
+  }
+
+
+
+  function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+  }
+
+
+
+  document.getElementById("demo").ondblclick = function() {myFunction()};
+
+  function myFunction() {
+    document.getElementById("demo").innerHTML = "I was double-clicked!";
+  }
+
+
+
+  function mouseDown() {
+    document.getElementById("demo").innerHTML = "The mouse button is held down.";
+  }
+  
+  function mouseUp() {
+    document.getElementById("demo").innerHTML = "You released the mouse button.";
+  }
+
+
+
+  function myFunction() 
+    alert("Thank you for visiting W3Schools!");
+
+
+  function myFunction() {
+      alert("The form was submitted");
+    }
+
+
+  function myFunction() {
+      alert("The form was reset");
+    }
+
+
+
+  function myFunction() 
+    alert("Thank you for visiting W3Schools!");
+
+
+    function myFunction() {
+      alert("You have changed the size of the browser window!");
+    }
